21093aa3ae478c6c48ba1e4593a02e286d1c2d3a
aanchalchopra <aanchal.a.chopra@accenture.com>
2020-07-19 11:22:21 +0530

    Intial Set Up
    Project set up

---
+++ SampleDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SampleDemo
+//
+//  Created by Atlas on 19/07/20.
+//  Copyright © 2020 Atlas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
